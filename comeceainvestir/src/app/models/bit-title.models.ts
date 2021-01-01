a37c872d580d27d5fa550c4cbf04bfb545aa9222
Uezima <guilherme@uezima.com>
2021-01-01 13:07:15 -0300

    feat: add style for home banner

--- comeceainvestir/src/app/models/bit-title.models.ts
+++ comeceainvestir/src/app/models/bit-title.models.ts
@@ -1,6 +1,7 @@
 export interface BigTitle {
   imageUrl: string,
   imageAlt: string,
+  imageAnimation?: string,
   title: string,
   subtitle: string,
   typewriter?: string[],
--- comeceainvestir/src/app/pages/landing/home/home.component.ts
+++ comeceainvestir/src/app/pages/landing/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BigTitle } from 'src/app/models/bit-title.models';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  
+  bigTitle: BigTitle = {
+    imageUrl: 'assets/pages/home/astronauta.png',
+    imageAlt: 'Desenho de um astronauta com moedas e notas de dinheiro ao redor dele no espaço',
+    imageAnimation: 'floating',
+    title: 'Treine seu lado investidor concorrendo a prêmios!',
+    subtitle: 'Simule investimentos na bolsa de valores em uma competição divertida onde aquele que fizer as melhores escolhas ganhará um prêmio!',
+    buttonAction: 'signin',
+    buttonText: 'Clique aqui para começar!'
+  }
 }
