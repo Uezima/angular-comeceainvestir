568feac184a94890342e57c3d7988cc3c9d638d7
Uezima <guilherme@uezima.com>
2021-01-01 02:09:45 -0300

    feat: add routing system

--- comeceainvestir/src/app/app-routing.module.ts
+++ comeceainvestir/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingLayoutComponent } from './layouts/landing/landing-layout.component';
+import { LandingRoutingModule } from './pages/landing/landing-routing.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingLayoutComponent,
+    children: [
+      { path: '', loadChildren:() => LandingRoutingModule }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- comeceainvestir/src/app/app.module.ts
+++ comeceainvestir/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/landing/home/home.component';
+import { LandingRoutingModule } from './pages/landing/landing-routing.module';
+import { LandingLayoutComponent } from './layouts/landing/landing-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LandingLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LandingRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
