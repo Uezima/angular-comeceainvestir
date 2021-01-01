d638523391140c2136ecf74fb125ce9b6d74f687
Uezima <guilherme@uezima.com>
2021-01-01 02:18:28 -0300

    feat: add footer component

--- comeceainvestir/src/app/app.module.ts
+++ comeceainvestir/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from './pages/landing/home/home.component';
 import { LandingRoutingModule } from './pages/landing/landing-routing.module';
 import { LandingLayoutComponent } from './layouts/landing/landing-layout.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LandingLayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
